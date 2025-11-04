35a4cf8af59650b8beba57538fa258e7856369d5
Adrián Gabás <adriangabasandreu@gmail.com>
2025-11-04 20:01:59 +0100

    Cambios

---
+++ Ejercicios_Programacion/src/Ejercicio7.java
@@ -0,0 +1,18 @@
+void main() {
+    Scanner teclado = new Scanner(System.in);
+    IO.println("Introduce una hora: ");
+    int hora = teclado.nextInt();
+    IO.println("Introduce minutos: ");
+    int minutos = teclado.nextInt();
+    IO.println("Introduce segundos: ");
+    int segundos = teclado.nextInt();
+
+    //TODO comprobar si la hora es correcta
+    boolean esValida = false;
+
+    //TODO si alguno de los datos no se cumple, el programa tiene que pedirlo
+
+    //TODO cuando sea valido debe mostrar la hora correcta
+
+
+    }
